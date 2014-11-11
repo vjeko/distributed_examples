f36be6c7498146f77511f5688613157981e02db1
Vjekoslav Brajkovic <vjeko@brajkovic.org>
2014-11-11 13:55:43 -0800

    Merge Panda's changes.

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -4,11 +4,12 @@ import broadcast.FireStarter
 import akka.actor.{ Actor, ActorRef, DeadLetter }
 import akka.actor.ActorSystem
 import akka.actor.Props
-
+import akka.dispatch.verification._
 
 
 object Main extends App {
-  
+  Instrumenter().scheduler = new akka.dispatch.verification.Scheduler
+ 
   val system = ActorSystem("Broadcast")
   val fireStarter = system.actorOf(
       Props(new FireStarter(system)),
