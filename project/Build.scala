8248efdab6b00d7d97cc350b0975647ef71723a1
Vjekoslav Brajkovic <vjeko@brajkovic.org>
2014-11-17 11:45:12 -0800

    Add Cassovary to the repo.

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,12 @@ object ConcurrencyeBuild extends Build {
       organization := "com.typesafe.sbt.aspectj",
       version := "0.1",
       scalaVersion := "2.11.2",
+
+      //resolvers += "twitter" at "http://maven.twttr.com",
+
       libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.6",
+      libraryDependencies += "com.twitter" % "cassovary_2.10" % "3.2.0",
+      libraryDependencies += "it.unimi.dsi" % "fastutil" % "6.4.4",
 
       // add akka-actor as an aspectj input (find it in the update report)
       inputs in Aspectj <++= update map { report =>
