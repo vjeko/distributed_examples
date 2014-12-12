cf3d0de58a4fc82b728f6124ff55de8934fddd08
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2014-12-11 21:52:54 -0800

    External events.

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,20 +1,40 @@
 
-import broadcast.FireStarter
+import broadcast.Node,
+       broadcast.Init,
+       broadcast.RB_Broadcast,
+       broadcast.DataMessage,
+       broadcast.FireStarter
 
-import akka.actor.{ Actor, ActorRef, DeadLetter }
-import akka.actor.ActorSystem
-import akka.actor.Props
-import akka.dispatch.verification._
+import akka.actor.Actor,
+       akka.actor.ActorRef,
+       akka.actor.DeadLetter,
+       akka.actor.ActorSystem,
+       akka.actor.Props
+       
+import akka.dispatch.verification.Instrumenter,
+       akka.dispatch.verification.ExternalEvent,
+       akka.dispatch.verification.Start,
+       akka.dispatch.verification.Send,
+       akka.dispatch.verification.DPOR
+
+import scala.collection.immutable.Vector
+
 
 
 object Main extends App {
-  Instrumenter().scheduler = new akka.dispatch.verification.DPOR
+
+  val scheduler = new DPOR
+  Instrumenter().scheduler = scheduler
   Instrumenter().tellEnqueue = new akka.dispatch.verification.TellEnqueueBusyWait
- 
-  val system = ActorSystem("Broadcast")
-  val fireStarter = system.actorOf(
-      Props(new FireStarter(system)),
-      name="FS")
-  fireStarter ! 0
   
+  val names = List.range(0, 3).map(i => "A-" + i.toString())
+  
+  val spawns = names.map(i => Start(Props[Node], name = i))
+  val inits = names.map(name => Send(name, Init(names.toSet)))
+  val rb = Send(names(0), RB_Broadcast(DataMessage(1, "Message")))
+      
+  val externalEvents : Vector[ExternalEvent] = Vector() ++
+    spawns ++ inits :+ rb
+  
+  scheduler.run(externalEvents)
 }
